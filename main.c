36d16dd46198693824772264739c3e95d3be5c2d
filipe-calasans <devnull@localhost>
2015-01-07 16:36:32 -0200

    ADC configured, TACT inputs configured, Ignition IOS configured and CPU LEDS configured.

--- main.c
+++ main.c
@@ -2,16 +2,20 @@
 
 #include "usart/usart.h"
 #include "multitask/multitask.h"
+#include "ios/ios.h"
 
 int main(void)
 {
-
 
 	initUSART();
 
 	initMultiTask();
 
+	initIOs();
+
     while(1)
     {
+    	executeEveryInterval(0, 1000, &toggleCPULED);
+
     }
 }
